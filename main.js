6a25c870151a150b5e4e0c6b13463dca3f243da4
techplacew <80680103+techplacew@users.noreply.github.com>
2021-03-15 20:00:53 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards', { delay: 500 });
+ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
+ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
